c7957481d9b85a7f0a0456a3d7ec21ac3a45199e
shivijain93 <61504164+shivijain93@users.noreply.github.com>
2020-04-14 22:33:20 +0530

    Add files via upload

---
+++ Angular-js/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MyAngularNewProj';
+  card_free={
+    card_type:'Free',
+    price:'$0',
+  options:[{name :'Single User',disable:false},
+  {name:'5GB Storage',disable:false},
+  {name:'Unlimited Public Projects',disable:false},
+  {name:'Community Access',disable:false},
+  {name:'Unlimited Private Projects',disable:true},
+  {name:'Dedicated Phone Support',disable:true},
+  {name:'Free Subdomain',disable:true},
+  {name:'Monthly Status Reports',disable:true},
+]
+
+};
+  card_plus={
+    'card_type':'Plus',
+  'price':'$9',
+  options:[{name :'5 Users',disable:false,isStrong:true},
+  {name:'50GB Storage',disable:false},
+  {name:'Unlimited Public Projects',disable:false},
+  {name:'Community Access',disable:false},
+  {name:'Unlimited Private Projects',disable:false},
+  {name:'Dedicated Phone Support',disable:false},
+  {name:'Free Subdomain',disable:false},
+  {name:'Monthly Status Reports',disable:true},
+]
+};
+  card_pro={
+    'card_type':'Pro',
+    'price':'$49',
+    options:[{name :'Unlimited Users',disable:false,isStrong:true},
+    {name:'150GB Storage',disable:false},
+    {name:'Unlimited Public Projects',disable:false},
+    {name:'Community Access',disable:false},
+    {name:'Unlimited Private Projects',disable:false},
+    {name:'Dedicated Phone Support',disable:false},
+    {name:'Free Subdomain',disable:false},
+    {name:'Monthly Status Reports',disable:false},
+  ]
+  };
+}
---
+++ Angular-js/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PriceCardComponent } from './price-card/price-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PriceCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
